03a98a531a45224ff2b784bc87a639d27dc6488d
giovanny hernandez <giovanni104@hotmail.com>
2023-02-03 16:32:53 -0500

    avances del curso

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,31 +1,37 @@
-
-import React, {useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs'
-
-export const GifGrid = ({category}) => {
-
-
-useEffect(() => {
-    getGifs(category);
-}, [ ])
-
-const [counter, setCounter] = useState(10);
-
-
+import { GifItem } from './GifItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
  
 
+export const GifGrid = ({ category }) => {
+  /*const [images, setImages] = useState([]);
 
-    return (    
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);*/
+
+
+  const { images, isLoading } = useFetchGifs( category );
+
+
+  return (
     <>
-    <h3>{category}</h3>
-    <p>Hola mundo</p>
+        <h3>{ category }</h3>
+            {
+                isLoading && ( <h2>Cargando...</h2> )
+            }
 
-    <h5>{counter}</h5>
-    <button onClick={()=> setCounter(counter+1)}>+1</button>
+      <div className="card-grid">
+        {images.map(( image ) => (
+          <GifItem key={image.id} {...image} />
+        ))
+        
+        }
+      </div>
     </>
-
-
-
-
-  )
-}
+  );
+};
