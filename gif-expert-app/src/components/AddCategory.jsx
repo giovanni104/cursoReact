abba478542cd1c17aa7824908ebb5bf24e521563
giovanny hernandez <giovanni104@hotmail.com>
2023-02-06 17:40:01 -0500

    avance del curso

--- gif-expert-app/src/components/AddCategory.jsx
+++ gif-expert-app/src/components/AddCategory.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react';
-
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
 
     const [ inputValue, setInputValue ] = useState('');
 
-   
     const onInputChange = ({ target }) => {
         setInputValue( target.value );
     }
 
-   
     const onSubmit = ( event ) => {
         event.preventDefault();
         if( inputValue.trim().length <= 1) return;
@@ -18,12 +16,10 @@ export const AddCategory = ({ onNewCategory }) => {
         // setCategories( categories => [ inputValue, ...categories ]);
         setInputValue('');
         onNewCategory( inputValue.trim() );
-      
     }
 
-
     return (
-        <form onSubmit={ onSubmit }>
+        <form onSubmit={ onSubmit } aria-label="form">
             <input 
                 type="text"
                 placeholder="Buscar gifs"
@@ -32,4 +28,10 @@ export const AddCategory = ({ onNewCategory }) => {
             />
         </form>
     )
+}
+
+
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
 }
