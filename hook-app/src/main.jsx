fbb031fd01d736bc7c5cbe6560a32d6541b342f8
giovanny hernandez <giovanni104@hotmail.com>
2023-02-07 17:26:55 -0500

    avance de hooks

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CounterApp } from './01-useState/CounterApp'
+import { Memorize } from './06-memos/Memorize'
+//import { Layout } from './05-useLayoutEffect/Layout'
+//import { FocusScreen } from './04-useRef/FocusScreen'
+//import { MultipleCustomHooks } from './03-examples'
+//import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+//import { SimpleForm } from './02-useEffect/SimpleForm'
+//import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+//import { CounterApp } from './01-useState/CounterApp'
 
 //import { HooksApp } from './HooksApp'
 
@@ -8,6 +15,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <CounterApp />
+   <Memorize/>
   </React.StrictMode>,
 )
