4f664fe5a44b9954c442c5de92bf9aaa025b7465
giovanny hernandez <giovanni104@hotmail.com>
2023-02-17 17:29:48 -0500

    avances

--- heroes-spa/src/heroes/components/HeroCard.jsx
+++ heroes-spa/src/heroes/components/HeroCard.jsx
@@ -11,7 +11,7 @@ export const HeroCard = ({
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
 
   return (
-    <div className="col">
+    <div className="col animate__animated animate__fadeIn">
       <div className="card"></div>
       <div className="row no-gutters">
         <div className="col-4">
@@ -22,7 +22,7 @@ export const HeroCard = ({
           <div className="card-body">
             <h5 className="card-title">{superhero}</h5>
             <p className="card-text">{alter_ego}</p>
-           
+
             {alter_ego !== characters && <p>characters</p>}
 
             <p className="card-text">
--- heroes-spa/src/heroes/components/HeroList.jsx
+++ heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,18 +1,21 @@
-import React from "react";
-
-import { getHeroesByPublisher } from "../helpers";
-import { HeroCard } from "./";
+import { useMemo } from 'react';
+import { HeroCard } from './';
+import { getHeroesByPublisher } from '../helpers';
 
 export const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
 
-  return (
-    <div className='row rows-cols-1 row-cols-md-3 g-3'>
-      {heroes.map((hero) => (
-       
-       <HeroCard key={hero.id} {...hero}  />
-      
-      ))}
-    </div>
-  );
-};
+    const heroes = useMemo( () => getHeroesByPublisher( publisher ), [ publisher ]);
+
+    return (
+        <div className="row rows-cols-1 row-cols-md-3 g-3">
+            {
+                heroes.map( hero => (
+                    <HeroCard 
+                        key={ hero.id } 
+                        { ...hero }
+                    />
+                ))
+            }
+        </div>
+    )
+}
