a037e8d9702fd71e3cbd6d094d50d2c1a3d85d9b
giovanny hernandez <giovanni104@hotmail.com>
2023-02-20 17:16:47 -0500

    final de seccion heroes spa

--- heroes-spa/src/router/AppRouter.jsx
+++ heroes-spa/src/router/AppRouter.jsx
@@ -1,17 +1,34 @@
-import {  Route, Routes } from "react-router-dom";
-import {  HeroesRoutes} from "../heroes"; 
+import { Route, Routes } from "react-router-dom";
+import { HeroesRoutes } from "../heroes";
 import { LoginPage } from "../auth";
- 
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
+
 export const AppRouter = () => {
   return (
     <>
-     
+      <Routes>
+        
 
-      <Routes>      
-        <Route path="login" element={<LoginPage />} />  
+        <Route
+          path="login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
 
-        <Route path="/*" element={<HeroesRoutes />} />  
-
+ 
+        
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </>
   );
