6709b8e9e427a658b4a852790446226386b4ba75
giovanny hernandez <giovanni104@hotmail.com>
2023-02-21 17:24:43 -0500

    prouecto journal app primer avance

---
+++ journal-app/src/auth/pages/LoginPage.jsx
@@ -0,0 +1,25 @@
+import { Grid } from "@mui/material"
+
+ 
+export const LoginPage = () => {
+  return (
+   
+    <Grid
+    container
+    spacing={0}
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{minHeight:'100vh',backgroundColor:'primary.main',padding:4}}
+    
+    >
+
+
+    </Grid>
+
+
+
+  )
+
+
+}
