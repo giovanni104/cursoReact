72a825d8ed5c435e084003023ae563cabccbec2e
giovanny hernandez <giovanni104@hotmail.com>
2023-02-24 17:30:18 -0500

    avance proyecto toolkit-redux

---
+++ toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,18 @@
+ 
+export const PokemonApp = () => {
+  return (
+   
+    <>
+    <h1>PokemonApp</h1>
+    <hr />
+
+    <ul>
+        <li>hola</li>
+        <li>hola</li>
+        <li>hola</li>
+    </ul>
+    
+    
+    </>
+  )
+}
---
+++ toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -0,0 +1,6 @@
+export const getPokemos=(page=0)=>{
+
+
+
+
+}
