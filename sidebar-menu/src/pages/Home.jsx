8e7d3bd1840b7814003f7c0c4a4f85b89294fdab
giovanny hernandez <giovanni104@hotmail.com>
2023-03-15 17:36:23 -0500

    avances

--- sidebar-menu/src/pages/Home.jsx
+++ sidebar-menu/src/pages/Home.jsx
@@ -8,12 +8,36 @@ import "./home.css";
 
 export const Home = () => {
   const [inputFields, setInputFields] = useState([
-    { firstName: "Gastos personales 0102***3245", lastName: "" },
+    {  cuenta: "Gastos personales 0102***3245",
+       banco: "none" ,
+       tipodoc:"",
+       numdoc:"",
+       telefono:"",
+       monto:"",
+       concepto:"",
+       fecha:"",
+       alias:"",
+       checkbox:false
+    },
   ]);
+
+  const [checked, setChecked] = useState(false);
+
+
 
   const handleAddFields = () => {
     const values = [...inputFields];
-    values.push({ firstName: "", lastName: "" });
+    values.push({  cuenta: "Gastos personales 0102***3245",
+       banco: "none" ,
+       tipodoc:"",
+       numdoc:"",
+       telefono:"",
+       monto:"",
+       concepto:"",
+       fecha:"",
+       alias:"",
+       checkbox:false
+    });
     setInputFields(values);
   };
 
@@ -25,11 +49,46 @@ export const Home = () => {
 
   const handleInputChange = (index, event) => {
     const values = [...inputFields];
-    if (event.target.name === "firstName") {
-      values[index].firstName = event.target.value;
-    } else {
-      values[index].lastName = event.target.value;
+console.log("aqui");
+    switch (event.target.name) {
+      case "cuenta":
+        values[index].cuenta = event.target.value;
+        break;
+      case "banco":
+        values[index].banco = event.target.value;
+        break;
+      case "tipodoc":
+        values[index].tipodoc = event.target.value;
+        break;
+      case "numdoc":
+        values[index].numdoc = event.target.value;
+        break;
+      case "telefono":
+        values[index].telefono = event.target.value;
+        break;
+      case "monto":
+        values[index].monto = event.target.value;
+        break;
+      case "concepto":
+        values[index].concepto = event.target.value;
+        break;
+      case "fecha":
+        values[index].fecha = event.target.value;
+        break;
+      case "alias":
+        values[index].firstName = event.target.value;
+        break;
+        case "checkbox":
+
+
+console.log(event.target.checked);
+
+          values[index].checkbox = event.target.checked;
+          break;
+        
+     
     }
+ 
 
     setInputFields(values);
   };
@@ -40,17 +99,45 @@ export const Home = () => {
     alert(JSON.stringify(inputFields, null, 2));
   };
 
-  const resetForm = (e) => setInputFields([{ firstName: "", lastName: "" }]);
+  const resetForm = (e) => setInputFields([{  cuenta: "Gastos personales 0102***3245",
+  banco: "none" ,
+  tipodoc:"",
+  numdoc:"",
+  telefono:"",
+  monto:"",
+  concepto:"",
+  fecha:"",
+  alias:"",
+  checkbox:false
+}]);
   const [age, setAge] = React.useState("");
 
   const handleChange = (event) => {
     setAge(event.target.value);
   };
 
+  const handleChangetChecked = () => {
+    setChecked(!checked);
+  };
+
+
+
   return (
     <>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <div className="divBloque">
+       
+
+        <>
+          <h1>Dynamic Form Fields in React</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="form-row">
+              {inputFields.map((inputField, index) => (
+                <Fragment key={`${inputField}~${index}`}>
+                  
+                
+
+
+                  <div className="divBloque">
           <Grid
             container
             rowSpacing={1}
@@ -61,16 +148,19 @@ export const Home = () => {
                 <input
                   type="text"
                   className="inputText"
-                  id="firstName"
-                  name="firstName"
-                  placeholder="Buscar gifs"
+                  id="cuenta"
+                  name="cuenta"
+                  value={inputField.cuenta}
+                  onChange={(event) => handleInputChange(index, event)}
                 />
                 <label>Saldo disponible: Bs. 4.325,45</label>
               </div>
             </Grid>
             <Grid item xs={6}>
               <div className="divInputs">
-                <select name="cars" id="cars" className="selectText">
+                <select name="banco"    id="banco" className="selectText"   value={inputField.banco}
+                                             onChange={(event) => handleInputChange(index, event)}>
+                <option  value="none"  disabled selected>Pago frecuente</option>
                   <option value="volvo">Volvo</option>
                   <option value="saab">Saab</option>
                   <option value="mercedes">Mercedes</option>
@@ -83,15 +173,19 @@ export const Home = () => {
             </Grid>
             <Grid item xs={6}>
               <div className="divCheckbox">
-                <input type="checkbox" name="cb-autos" value="gusta" />{" "}
+                <input  name="checkbox"    id="checkbox"    type="checkbox"   onChange={(event) => handleInputChange(index, event)}/>{" "}
                 <label>Beneficiario no registrado</label>
               </div>
             </Grid>
 
-            <Grid item xs={6}>
+
+
+{
+        inputField.checkbox  &&  <><Grid item xs={6}>
               <div style={{width: '86px', float:'left'}}>
-                <select name="tipo" id="tipo" className="selectTextBeneficiario">
-                <option disabled selected>Tipo</option>
+                <select   defaultValue={"none"}  className="selectTextBeneficiario" name="tipodoc"    id="tipodoc"  value={inputField.tipodoc}
+                                             onChange={(event) => handleInputChange(index, event)}>
+                <option  value="none"  disabled  >Tipo</option>
                   <option value="V">V</option>
                   <option value="E">E</option>
                   <option value="P">P</option>
@@ -103,63 +197,73 @@ export const Home = () => {
                 <input
                   type="text"
                   className="inputTextBeneficiario"
-                  id="firstName"
-                  name="firstName"
-                  placeholder="hhh"
+                  id="numdoc"
+                  name="numdoc"
+                  placeholder="Documento"
+                  onChange={(event) => handleInputChange(index, event)}
                 />
               </div>
             </Grid>
+
+
+
+            <Grid item xs={6}>
+              <div className="divInputs">
+                <input
+                  type="text"
+                  className="inputText"
+                  id="telefono"
+                  name="telefono"
+                  placeholder="Teléfono"
+                  onChange={(event) => handleInputChange(index, event)}
+                />
+               
+              </div>
+            </Grid>
+            </> 
+          }
+            <Grid item xs={6}>
+              <div className="divInputs">
+                <input
+                  type="text"
+                  className="inputText"
+                  id="monto"
+                  name="monto"
+                  placeholder="Monto(*)"
+                  onChange={(event) => handleInputChange(index, event)}
+                />
+                
+              </div>
+            </Grid>
+            <Grid item xs={6}>
+              <div className="divInputs">
+                <select name="concepto" id="concepto" className="selectText" value={inputField.concepto}
+                                             onChange={(event) => handleInputChange(index, event)}>
+                <option  value="none"  disabled selected>Concepto(*)</option>
+                  <option value="volvo">Volvo</option>
+                  <option value="saab">Saab</option>
+                  <option value="mercedes">Mercedes</option>
+                  <option value="audi">Audi</option>
+                </select>
+              </div>
+            </Grid>
+
+            
           </Grid>
         </div>
 
-        <>
-          <h1>Dynamic Form Fields in React</h1>
-          <form onSubmit={handleSubmit}>
-            <div className="form-row">
-              {inputFields.map((inputField, index) => (
-                <Fragment key={`${inputField}~${index}`}>
-                  <div className="divInputs">
-                    <input
-                      type="text"
-                      className="inputText"
-                      id="firstName"
-                      name="firstName"
-                      placeholder="Buscar gifs"
-                      value={inputField.firstName}
-                      onChange={(event) => handleInputChange(index, event)}
-                    />
-                    <label htmlFor="firstName">
-                      Saldo disponible: Bs. 4.325,45
-                    </label>
-                  </div>
-                  <div className="form-group col-sm-4">
-                    <label htmlFor="lastName">Last Name</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="lastName"
-                      name="lastName"
-                      value={inputField.lastName}
-                      onChange={(event) => handleInputChange(index, event)}
-                    />
-                  </div>
-                  <div className="form-group col-sm-2">
-                    <button
-                      className="btn btn-link"
-                      type="button"
-                      disabled={index === 0}
-                      onClick={() => handleRemoveFields(index)}
-                    >
-                      -
-                    </button>
-                    <button
+
+        <button
                       className="btn btn-link"
                       type="button"
                       onClick={() => handleAddFields()}
                     >
                       +
                     </button>
-                  </div>
+
+
+
+
                 </Fragment>
               ))}
             </div>
