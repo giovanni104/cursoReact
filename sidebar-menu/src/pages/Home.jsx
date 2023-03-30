761f345517922deace18cb0a41f55738d09c7322
giovanny hernandez <giovanni104@hotmail.com>
2023-03-30 17:30:06 -0500

    avance tabs

--- sidebar-menu/src/pages/Home.jsx
+++ sidebar-menu/src/pages/Home.jsx
@@ -1,8 +1,35 @@
-import React, { useState } from "react";
-import Box from "@mui/material/Box"; 
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
 import { DynamicForm } from "./DynamicForm";
+import "./home.css";
 
 export const Home = () => {
+  useEffect(() => {
+    const tabs = document.querySelectorAll("[data-tab-value]");
+    const tabInfos = document.querySelectorAll("[data-tab-info]");
+
+    tabs.forEach((tab) => {
+      tab.addEventListener("click", () => {
+        removeTabActive();
+        const target = document.querySelector(tab.dataset.tabValue);
+        const target_1 = document.querySelector(tab.dataset.tabValue + "h");
+
+        tabInfos.forEach((tabInfo) => {
+          tabInfo.classList.remove("active");
+        });
+        target.classList.add("active");
+        target_1.classList.add("act");
+      });
+    });
+  });
+
+  const removeTabActive = () => {
+    const tabs = document.querySelectorAll("[data-tab-value]");
+    tabs.forEach((tab) => {
+      const target = document.querySelector(tab.dataset.tabValue + "h");
+      target.classList.remove("act");
+    });
+  };
 
   return (
     <>
@@ -13,9 +40,37 @@ export const Home = () => {
         alignItems="center"
         justifyContent="center"
       >
-        <>
-        <DynamicForm></DynamicForm>
-        </>
+        <div className="tabs">
+          <div className="contenTabs">
+          <span id="tab_1h" className="act" data-tab-value="#tab_1">
+            Personas
+          </span>
+          </div>
+          <div className="contenTabs2">
+          <span id="tab_2h" data-tab-value="#tab_2">
+            Comercios
+          </span>
+          </div>
+          <div className="contenTabs">
+          <span id="tab_3h" data-tab-value="#tab_3">
+           Comunas
+          </span>
+          </div>
+        </div>
+
+        <div className="tab-content">
+          <div className="tabs__tab active" id="tab_1" data-tab-info>
+          <DynamicForm key={1}></DynamicForm>
+          </div>
+          <div className="tabs__tab" id="tab_2" data-tab-info>
+          <DynamicForm key={2}></DynamicForm>
+          </div>
+          <div className="tabs__tab" id="tab_3" data-tab-info>
+          <DynamicForm key={3}></DynamicForm>
+          </div>
+        </div>
+
+       
       </Box>
     </>
   );
