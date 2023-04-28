913cf09c0f27cc9de1144d1d2450c33319e651ea
Giovanny Hernandez Arbelaez <46624723+giovanni104@users.noreply.github.com>
2023-04-28 17:47:36 -0500

    update estado

--- sidebar-menu/src/context/MenuContext.jsx
+++ sidebar-menu/src/context/MenuContext.jsx
@@ -3,7 +3,7 @@ import React, {  useState } from "react";
 export const MenuContext = React.createContext({});
 export default function MenuContextProvider({ children }) {
   const [subMenu, setSubMenu] = useState(false);
-  const [menu, setMenu] = useState(true);
+  const [menu, setMenu] = useState(false);
   return (
     <MenuContext.Provider value={{ subMenu, setSubMenu,menu, setMenu }}>
       {children}
