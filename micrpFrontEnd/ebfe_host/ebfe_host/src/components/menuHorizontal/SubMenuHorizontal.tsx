4695b6f9981fe128e87edfb86a41ca7ee9c2fec2
giovanni104 <giovanni104@hotmail.com>
2023-05-07 23:56:35 -0500

    avances de migracion a typescript

---
+++ micrpFrontEnd/ebfe_host/ebfe_host/src/components/menuHorizontal/SubMenuHorizontal.tsx
@@ -0,0 +1,119 @@
+import * as React from "react";
+
+import Stack from "@mui/material/Stack";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { subMenuType } from "./typesMenuHorizontal"
+import "./horizontal.css";
+
+export const SubMenuHorizontal = (
+  handleClose: Function,
+  menuFields:any,
+  setMenuFields:any,
+) => {
+  //const [menuFields, setMenuFields] = React.useState([menuData]);
+
+  const hover = (id:number) => {
+    const menuValues = [...menuFields];
+    menuValues[id].stateImg = false;
+    setMenuFields(menuValues);
+  };
+
+  const unhover = (id:number)=> {
+    const menuValues = [...menuFields];
+    menuValues[id].stateImg = true;
+    setMenuFields(menuValues);
+  };
+
+  return (
+    <>
+      <List
+        id="submenu"
+        className="div_submenu"
+        component={Stack}
+        direction="row"
+      >
+        {menuFields.map((data:subMenuType, index:any) => {
+          return (
+            <ListItem key={index}             
+              onMouseOver={(event) => hover(index)}
+              onMouseOut={(event) => unhover(index)}
+              disablePadding
+              
+              sx={{
+                width: "250px",
+                maxHeight: "50px",
+              
+              }}
+              onClick={(event) => handleClose()}
+            >
+              <ListItemButton   key={index}
+                sx={{
+                  height: "55px",
+                  backgroundColor: "#FFFFFF",
+                  borderRadius: "8px 8px 0px 0px",
+               
+                  ":hover": {
+                    color: "white",
+                    backgroundColor: "#4A96D2",
+                    borderRadius: "8px 8px 0px 0px",
+                    height: "55px",
+                 
+
+                    
+                  },
+                }}
+              >
+                <ListItemIcon  key={index}
+                  sx={{
+                    minWidth: 0,
+                    mr: "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  <img
+                    id={index}
+                    src={data.stateImg ? data.img1 : data.img2}
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      left: "8px",
+                      top: "10px",
+                    }}
+                  />
+                </ListItemIcon>
+                <ListItemText sx={{marginLeft:"10px" ,width:""}}
+                  primary={data.text}
+                  primaryTypographyProps={{
+                    fontSize: "16px",
+                    fontFamily: "Nunito",
+                    fontWeight: "400",
+                    lineHeight: "22px",
+                    letterSpacing: "0em",
+                    textAlign: "left",
+                  }}
+                />
+
+                {!data.stateImg  && (
+                  <ArrowForwardIcon
+                    id="prueba_row"
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      left: "8px",
+                      top: "10px",
+                    }}
+                  />
+                )}
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+    </>
+  );
+};
