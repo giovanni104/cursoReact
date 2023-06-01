fb88f00ecfe9c1097e1ee9dda6cbe30118bf7a70
giovanni104 <giovanni104@hotmail.com>
2023-05-31 20:46:01 -0500

    ajustes header de chat bot

--- sidebar-menu/src/reactChatBotKit/chatbot/componentes/CustomHeader.jsx
+++ sidebar-menu/src/reactChatBotKit/chatbot/componentes/CustomHeader.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'
+import Brightness1Icon from '@mui/icons-material/Brightness1'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box';
 
- 
+
 const CustomHeader = () => {
   return (
-    <div class="react-chatbot-kit-chat-header">¿Qué deseas hacer 2?</div>
-  );
-};
+    <>
+      <div className="react-chatbot-kit-chat-header">
+        <Grid container spacing={2}>
+          <Grid item xs={2}>
+            <Brightness1Icon sx={{ color: '#04BB37' }} />
+          </Grid>
+          <Grid item xs={8}>
+          <Box display="flex" justifyContent="left">
+            <Typography
+            align='left'
+              fontFamily={'Nunito'}
+              fontSize={'20px'}
+              color={'#4A96D2'}
+              fontWeight={'700'}
+              fontStyle={'normal'}
+            >
+              ¿Qué deseas hacer?
+            </Typography>
 
-export default CustomHeader;
+            </Box>
+
+          </Grid>
+
+          <Grid   xs={2}  item  >
+          <Box display="flex" justifyContent="flex-end">
+            <HighlightOffIcon
+              style={{   color: '#4A96D2', width: '34px', height: '34px' }}
+            />
+             </Box>
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  )
+}
+
+export default CustomHeader
