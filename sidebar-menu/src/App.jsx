e2eabb836c303565f13034b6c51f2789849e18d4
Giovanny Hernandez Arbelaez <giovanni104@hotmail.com>
2023-06-26 22:18:58 -0500

    formulario dinamico

--- sidebar-menu/src/App.jsx
+++ sidebar-menu/src/App.jsx
@@ -4,14 +4,18 @@ import { About } from "./pages/About";
 import { Settings } from "./pages/Settings";
 import { Sidenav } from "./menu/Sidenav";
 import MenuContextProvider from "./context/MenuContext";
-import  {Horizontal} from "./Horizontal";
-import {Chat} from "./reactChatBotKit/index"
+
 function App() {
   return (
     <>
-<Chat></Chat>
-
- 
+     <MenuContextProvider>
+      <Sidenav />
+      </MenuContextProvider>
+      <Routes>
+        <Route path="/" exact element={<Home />}></Route>
+        <Route path="/about" exact element={<About />}></Route>
+        <Route path="/settings" exact element={<Settings />}></Route>
+      </Routes>
     </>
   );
 }
